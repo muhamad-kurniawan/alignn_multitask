dd9545c908ad50444ae8c28def828f8ec6b6e531
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2024-09-09 11:19:39 +0900

    Update alignn_multitask.py

--- alignn/models/alignn_multitask.py
+++ alignn/models/alignn_multitask.py
@@ -428,3 +428,105 @@ class ALIGNNMT(nn.Module):
         # return torch.squeeze(out)
         return outputs
 
+class Normalizer:
+    """Normalize a Tensor and restore it later."""
+
+    def __init__(self) -> None:
+        """Initialize Normalizer with mean 0 and std 1."""
+        self.mean = torch.tensor(0)
+        self.std = torch.tensor(1)
+
+    def fit(self, tensor: Tensor, dim: int = 0, keepdim: bool = False) -> None:
+        """Compute the mean and standard deviation of the given tensor.
+
+        Args:
+            tensor (Tensor): Tensor to determine the mean and standard deviation over.
+            dim (int, optional): Which dimension to take mean and standard deviation
+                over. Defaults to 0.
+            keepdim (bool, optional): Whether to keep the reduced dimension in Tensor.
+                Defaults to False.
+        """
+        self.mean = torch.mean(tensor, dim, keepdim)
+        self.std = torch.std(tensor, dim, keepdim)
+
+    def norm(self, tensor: Tensor) -> Tensor:
+        """Normalize a Tensor.
+
+        Args:
+            tensor (Tensor): Tensor to be normalized
+
+        Returns:
+            Tensor: Normalized Tensor
+        """
+        return (tensor - self.mean) / self.std
+
+    def denorm(self, normed_tensor: Tensor) -> Tensor:
+        """Restore normalized Tensor to original.
+
+        Args:
+            normed_tensor (Tensor): Tensor to be restored
+
+        Returns:
+            Tensor: Restored Tensor
+        """
+        return normed_tensor * self.std + self.mean
+
+    def state_dict(self) -> dict[str, Tensor]:
+        """Get Normalizer parameters mean and std.
+
+        Returns:
+            dict[str, Tensor]: Dictionary storing Normalizer parameters.
+        """
+        return {"mean": self.mean, "std": self.std}
+
+    def load_state_dict(self, state_dict: dict[str, Tensor]) -> None:
+        """Overwrite Normalizer parameters given a new state_dict.
+
+        Args:
+            state_dict (dict[str, Tensor]): Dictionary storing Normalizer parameters.
+        """
+        self.mean = state_dict["mean"].cpu()
+        self.std = state_dict["std"].cpu()
+
+    @classmethod
+    def from_state_dict(cls, state_dict: dict[str, Tensor]) -> Normalizer:
+        """Create a new Normalizer given a state_dict.
+
+        Args:
+            state_dict (dict[str, Tensor]): Dictionary storing Normalizer parameters.
+
+        Returns:
+            Normalizer
+        """
+        instance = cls()
+        instance.mean = state_dict["mean"].cpu()
+        instance.std = state_dict["std"].cpu()
+
+        return instance
+
+
+def sampled_softmax(pre_logits: Tensor, log_std: Tensor, samples: int = 10) -> Tensor:
+    """Draw samples from Gaussian distributed pre-logits and use these to estimate
+    a mean and aleatoric uncertainty.
+
+    Args:
+        pre_logits (Tensor): Expected logits before softmax.
+        log_std (Tensor): Deviation in logits before softmax.
+        samples (int, optional): Number of samples to take. Defaults to 10.
+
+    Returns:
+        Tensor: Averaged logits sampled from pre-logits
+    """
+    # NOTE here as we do not risk dividing by zero should we really be
+    # predicting log_std or is there another way to deal with negative numbers?
+    # This choice may have an unknown effect on the calibration of the uncertainties
+    sam_std = torch.exp(log_std).repeat_interleave(samples, dim=0)
+
+    # TODO here we are normally distributing the samples even if the loss
+    # uses a different prior?
+    epsilon = torch.randn_like(sam_std)
+    pre_logits = pre_logits.repeat_interleave(samples, dim=0) + torch.mul(
+        epsilon, sam_std
+    )
+    logits = softmax(pre_logits, dim=1).view(len(log_std), samples, -1)
+    return torch.mean(logits, dim=1)
